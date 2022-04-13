fbafa3ec34f3e4dd45648249ab7e85e169e5f6c6
Victor Stassun <victorstassun@gmail.com>
2022-04-13 17:16:58 -0300

    Front-end starting develompent

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+
+  <MenuComponent />
+
+  </div>
+</template>
+
+<script>
+import MenuComponent from './components/menu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MenuComponent,
+  }
+}
+</script>
---
+++ src/components/menu.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>oi</h1>
+    </div>
+</template>
+    
+<script>
+    export default {
+        name: 'MenuComponent',
+    }
+</script>
+
+<style scoped>
+
+</style>
+
+
